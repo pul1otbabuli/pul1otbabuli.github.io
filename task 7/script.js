4e77dc91eae42bf3a81d74690bdc55360677e374
pul1otbabuli <145927304+pul1otbabuli@users.noreply.github.com>
2023-11-12 23:56:35 +0300

    script.js

--- task 7/script.js
+++ task 7/script.js
@@ -1,18 +1,39 @@
-$(document).ready(function(){
-    $('.gallery').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-        nextArrow: '<button type="button" class="slick-next">Next</button>',
-        dots: false,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector(".gallery");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  const pageNumber = document.querySelector(".page-number");
+
+  let currentPage = 1;
+  const itemsPerPage = 3;
+
+  const totalItems = document.querySelectorAll(".slider img").length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  updatePager();
+
+  prevButton.addEventListener("click", function () {
+    if (currentPage > 1) {
+      currentPage--;
+      updateGallery();
+      updatePager();
+    }
+  });
+
+  nextButton.addEventListener("click", function () {
+    if (currentPage < totalPages) {
+      currentPage++;
+      updateGallery();
+      updatePager();
+    }
+  });
+
+  function updateGallery() {
+    const translateValue = -((currentPage - 1) * 100) + "%";
+    gallery.style.transform = "translateX(" + translateValue + ")";
+  }
+
+  function updatePager() {
+    pageNumber.textContent = currentPage + " / " + totalPages;
+  }
 });
